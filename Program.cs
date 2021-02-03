e408f0588ceb53cade376b5bacd20c1c412d8cd4
rzientara <rzientara96@gmail.com>
2021-02-03 15:14:49 -0600

    added name method

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ namespace console_app
         static void Main(string[] args)
         {
             Console.WriteLine("Choose a program.\n[1] name\n[2] area\n[3] mpg\n[4] string search\n[q] quit");
+            string option = Console.ReadLine();
+            if (option == "1")
+            {
+                Name();
+            }
+        }
+
+        static void Name()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
             Console.ReadLine();
         }
     }
