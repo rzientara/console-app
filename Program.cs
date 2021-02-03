0662601f7cb2d12f920f6ebf31cc68647245062c
rzientara <rzientara96@gmail.com>
2021-02-03 14:51:00 -0600

    added menu display

--- Program.cs
+++ Program.cs
@@ -2,11 +2,13 @@
 
 namespace console_app
 {
-    class Program
+    class Menu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Choose a program.\n[1] name\n[2] area\n[3] mpg\n[4] string search\n[q] quit");
+            Console.ReadLine();
         }
     }
+
 }
